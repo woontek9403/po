1e5752a49f16a1e5ad4dc165cd7d6b6e652257af
Unknown <woontek9403@naver.com>
2018-07-11 09:39:43 +0900

    po2 예제

---
+++ po2/js/main.js
@@ -0,0 +1,64 @@
+
+// 그래픽카드
+$('.btn1').click(function(e) { 
+  jQuery( ".card" ).animate({right : 0}, 300 ); 
+
+}); 
+$('.card').mouseleave(function(e) {
+  jQuery( ".card" ).animate({right: '-300px'}, 300 );
+}); 
+
+
+// 모니터
+$('.btn2').click(function(e) { 
+  jQuery( ".monitor" ).animate({right : 0}, 300 ); 
+});
+$('.monitor').mouseleave(function(e) {
+  jQuery( ".monitor" ).animate({right: '-300px'}, 300 );
+});
+
+
+// 렘
+$('.btn3').click(function(e) { 
+  jQuery( ".rem" ).animate({right : 0}, 300 ); 
+});  
+$('.rem').mouseleave(function(e) {
+  jQuery( ".rem" ).animate({right: '-300px'}, 300 );
+}); 
+
+
+// 파워
+$('.btn4').click(function(e) { 
+  jQuery( ".power" ).animate({right : 0}, 300 ); 
+});
+$('.power').mouseleave(function(e) {
+  jQuery( ".power" ).animate({right: '-300px'}, 300 );
+});
+
+
+// cpu
+$('.btn5').click(function(e) { 
+  jQuery( ".cpu" ).animate({right : 0}, 300 ); 
+});  
+$('.cpu').mouseleave(function(e) {
+  jQuery( ".cpu" ).animate({right: '-300px'}, 300 );
+}); 
+
+
+// 본체
+$('.btn6').click(function(e) { 
+  jQuery( ".case" ).animate({right : 0}, 300 ); 
+});
+$('.case').mouseleave(function(e) {
+  jQuery( ".case" ).animate({right: '-300px'}, 300 );
+});
+
+
+// 메인보드
+$('.btn7').click(function(e) { 
+  jQuery( ".board" ).animate({right : 0}, 300 ); 
+}); 
+$('.board').mouseleave(function(e) {
+  jQuery( ".board" ).animate({right: '-300px'}, 300 );
+}); 
+
